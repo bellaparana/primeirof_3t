446a12ef8a0fda00973ab508079079c7633b984e
bellaparana <111066478+bellaparana@users.noreply.github.com>
2022-10-06 11:13:34 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,7 +1,9 @@
-let tabuada = 2;
+let tabuada = 5;
 
-function escreva(){ 
-    document.write("Tabuda do " + tabuada + "<br>")
+
+
+function escreva(){
+    document.write("Tabuada do " + tabuada + "<br>")
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
     document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
@@ -12,6 +14,19 @@ function escreva(){
     document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
     document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+}
+let lista = ["Jorge", "Duarte", "Waldete", "Raquel",];
+
+function mostra(){
+document.write(lista.lenght + "<br>"); 
+for(let i = 0; i < lista.lenght; i++){
+    document.write(lista[i] + "<br>");
+}
+
+function mostraTabuada(){
+    for(let i = 1; i <= 10; i++)
+    document.write("O valor do i " + i + "<br>");
+}
 
 }
 
